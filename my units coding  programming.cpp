531bfafadbb4646c9c137a453bef2fb84b1a0987
CT100129 <gichuhi.26129@students.kyu.ac.ke>
2025-09-21 05:23:52 -0700

    Add files via upload

---
+++ my units coding  programming.cpp
@@ -0,0 +1,25 @@
+//simple c program(bsc IT units) single line
+/*
+Name:joseph kariuki
+Geg:CT100/G/26129/25
+Description:semester 1 year 1 units
+*/
+#include <stdio.h>//pre-processor directive standard input output
+
+//main function
+int main() {
+   printf(" \t  All semister 1 yr 1 units:\n");
+   printf("1.mathematics for science\n");
+   printf("2.introduction to computer applications\n"); 
+   printf("3.analog elctronics\n");
+   printf("4.introduction to programing and algorithms\n");
+   printf("5.fundanmentals of ICT\n");
+   printf("6.computer systems and organization\n");
+   printf("7.communication and information literacy skills\n");
+   printf("8.essetials of accounting\n");
+   return'0';
+}
+
+
+   
+   
