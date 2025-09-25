2887b92718d703989e5ebacb2ae26759c81499d2
CT100129 <gichuhi.26129@students.kyu.ac.ke>
2025-09-25 09:57:56 -0700

    Add files via upload

---
+++ eligibility of a loan code.cpp
@@ -0,0 +1,33 @@
+//simple c program(eligibility of a loan)simple line comment
+/*
+Name:JOSEPH KARIUKI GICHUHI
+Reg:CT100/G/26129/25
+Desciption;eligibilty of a loan
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int age;    
+    double income;    
+
+    // Get user input for age
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    // Get user input for income
+    printf("Enter your annual income: ");
+    scanf("%lf", &income);
+
+    // Check eligibility
+    if (age >= 21 & income >= 21000) 
+        printf("\nCongratulations, you qualify for a loan.\n");
+     else 
+        printf("\nUnfortunately, we are unable to offer you a loan at this time.\n");
+    
+
+    return 0;
+}
+	
+
+
