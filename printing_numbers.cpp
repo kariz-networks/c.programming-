836efa2a54d37c70865a657697da86cc6e3f7b7f
kariz-networks <gichuhi.26129@students.kyu.ac.ke>
2025-10-05 12:32:55 +0300

    Add files via upload

---
+++ printing_numbers.cpp
@@ -0,0 +1,17 @@
+// simple c program
+/*
+Name:joseph kariuki gichuhi
+Reg:CT100/G/26129/25
+Description: print numbers 100 to 50
+
+*/
+#include <stdio.h>
+int main() {
+   //start ,stop,step
+   int i;  //start
+   for(i=100; i>=50; i--){
+   	printf("%d  \n",  i);
+}
+	
+return 0;
+}
